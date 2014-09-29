0ab9db8c1d0d76174ad7961d40ba6451adac6d1b
hunt <hunt.b.graham@gmail.com>
2014-09-28 21:28:07 -0700

    Added graphics class

--- app/src/main/java/com/apps/hgb/bananagrams/GameBoard.java
+++ app/src/main/java/com/apps/hgb/bananagrams/GameBoard.java
@@ -14,45 +14,23 @@ import android.view.View;
 public class GameBoard extends View {
 
     GameActivity gameActivity;
-
-    Paint backgroundColor;
-    Paint blockColor;
+    Graphics g;
 
     public GameBoard(Context context) {
         super(context);
 
-        backgroundColor = new Paint();
-        blockColor = new Paint();
-        backgroundColor.setColor(Color.BLUE);
-        blockColor.setColor(Color.BLACK);
-        blockColor.setStrokeWidth(5f);
-
+        g = new Graphics();
+        this.gameActivity = (GameActivity) context;
 
         setFocusable(true);
         setFocusableInTouchMode(true);
 
-        this.gameActivity = (GameActivity) context;
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
-        int numSquares = 8;
-        for (int i = 1; i < numSquares + 1; i++)
-        {
-            float startX = getWidth() * i / numSquares;
-            float startY = 0;
-            float endX = getWidth() * i / numSquares;
-            float endY = getHeight();
-            canvas.drawLine(startX, startY, endX, endY, blockColor);
 
-            startX = 0;
-            startY = getHeight() * i / numSquares;
-            endX = getWidth();
-            endY = getHeight() * i / numSquares;
-            canvas.drawLine(startX, startY, endX, endY, blockColor);
-        }
-
-        //canvas.drawLine(50, 0, 50, 500, blockColor);
+        g.Draw(canvas, getHeight(), getWidth());
 
         super.onDraw(canvas);
     }
---
+++ app/src/main/java/com/apps/hgb/bananagrams/Graphics.java
@@ -0,0 +1,44 @@
+package com.apps.hgb.bananagrams;
+
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+
+/**
+ * Created by Hunt on 9/28/2014.
+ */
+public class Graphics {
+
+    //
+    //
+    //
+    Paint gridColor = new Paint();
+
+    public Graphics()
+    {
+        gridColor.setColor(Color.BLACK);
+        gridColor.setStrokeWidth(5f);
+    }
+
+    public void Draw(Canvas canvas, int height, int width)
+    {
+        int numSquares = 8;
+        float startX, startY, endX, endY = 0f;
+
+        for (int i = 1; i < numSquares + 1; i++)
+        {
+            startX = width * i / numSquares;
+            startY = 0;
+            endX = width * i / numSquares;
+            endY = height;
+            canvas.drawLine(startX, startY, endX, endY, gridColor);
+
+            startX = 0;
+            startY = height * i / numSquares;
+            endX = width;
+            endY = height * i / numSquares;
+            canvas.drawLine(startX, startY, endX, endY, gridColor);
+        }
+
+    }
+}
