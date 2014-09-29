2af64a0bf8ea0f3599db76b1fe7576f0c8ba52f6
hunt <hunt.b.graham@gmail.com>
2014-09-28 19:52:01 -0700

    Added grid lines

--- app/src/main/java/com/apps/hgb/bananagrams/GameBoard.java
+++ app/src/main/java/com/apps/hgb/bananagrams/GameBoard.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Rect;
 import android.view.MotionEvent;
 import android.view.View;
 
@@ -14,10 +15,17 @@ public class GameBoard extends View {
 
     GameActivity gameActivity;
 
-    Paint backgroundColor = new Paint(Color.CYAN);
+    Paint backgroundColor;
+    Paint blockColor;
 
     public GameBoard(Context context) {
         super(context);
+
+        backgroundColor = new Paint();
+        blockColor = new Paint();
+        backgroundColor.setColor(Color.BLUE);
+        blockColor.setColor(Color.YELLOW);
+
 
         setFocusable(true);
         setFocusableInTouchMode(true);
@@ -27,7 +35,31 @@ public class GameBoard extends View {
 
     @Override
     protected void onDraw(Canvas canvas) {
-        canvas.drawRect(0, 0, getWidth(), getHeight(), backgroundColor);
+
+        //canvas.drawRect(0, 0, getWidth(), getHeight(), backgroundColor);
+
+        //
+        canvas.drawRect(0, 0, getWidth()/8f, getHeight()/8f, blockColor);
+
+        int numSquares = 8;
+        for (int i = 1; i < numSquares + 1; i++)
+        {
+            // drawLine(float startX, float startY, float stopX, float stopY, Paint paint)
+            //float startPoint =
+            float startX = getWidth() / numSquares;
+            //float startY = getHeight() / numSquares;
+            float startY = 0;
+            float endX = getWidth() / numSquares;
+            float endY = getHeight();
+            canvas.drawLine(startX, startY, endX, endY, blockColor);
+
+            //canvas.drawLine(getWidth()/numSquares, 0, getWidth()/numSquares, getHeight(), blockColor);
+        }
+
+        blockColor.setStrokeWidth(5);
+
+        canvas.drawLine(50, 0, 50, 500, blockColor);
+
         super.onDraw(canvas);
     }
 
