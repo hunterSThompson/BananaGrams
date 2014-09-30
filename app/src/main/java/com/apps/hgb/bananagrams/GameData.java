cf375f314f0c0e32255612fdffa3766bbaf35a4c
hunt <hunt.b.graham@gmail.com>
2014-09-29 19:09:39 -0700

    Added some plumbing.

--- app/src/main/java/com/apps/hgb/bananagrams/GameData.java
+++ app/src/main/java/com/apps/hgb/bananagrams/GameData.java
@@ -1,5 +1,6 @@
 package com.apps.hgb.bananagrams;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 
 /**
@@ -7,7 +8,31 @@ import java.util.ArrayList;
  */
 public class GameData {
 
-    public ArrayList< ArrayList<String>> wholeGrid;
-    public int time;
-    public ArrayList<String> tiles;
+    public GameTile[][] gameTiles = new GameTile[100][100];
+
+    public String[] selectableTiles = new String[50];
+
+    public Range xRange;
+    public Range yRange;
+
+    public String serialize()
+    {
+        return "";
+    }
+
+    // TODO: Implement GameData constructor.  Should randomly generate tiles
+    public GameData()
+    {
+    }
+
+    // TODO: Implement resume game constructor.  Should call deserialize function.
+    public GameData(String data)
+    {
+    }
+
+    // TODO: Implement deserialze func
+    private GameData deserialize(String data)
+    {
+        return null;
+    }
 }
---
+++ app/src/main/java/com/apps/hgb/bananagrams/Range.java
@@ -0,0 +1,9 @@
+package com.apps.hgb.bananagrams;
+
+/**
+ * Created by Hunt on 9/29/2014.
+ */
+public class Range {
+    public int start;
+    public int end;
+}
